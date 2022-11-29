5986740389a364347bf95b806473158d365dcf42
zouhai <zouhai@stary.com>
2022-11-29 17:31:58 +0800

    feat: 开放静态资源/uploads

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import cors from 'cors';
 import bodyParser from "body-parser";
+import path  from 'path';
 import MockRoute from "./routes/index.js";
 import UploadRoute from "./routes/upload.js";
 
 const app = express();
+const __dirname = path.resolve();
 
 /**
  * 启用所有cors请求
@@ -24,6 +26,8 @@ app.use(bodyParser.urlencoded({
 }));
 // 注册路由
 app.use(MockRoute, UploadRoute);
+//开放静态资源
+app.use('/uploads', express.static(__dirname + '/uploads'));
 
 /* 监听端口 */
 app.listen(3000, () => {
